5a503c61067b401876df85bf5cfc49b75bea5c59
Impolite Goofball <github.dismiss894@passmail.net>
2025-08-18 15:43:41 +0930

    accept a client connection from the server

--- src/main/java/net/ethandankiw/Main.java
+++ src/main/java/net/ethandankiw/Main.java
@@ -3,6 +3,7 @@ package net.ethandankiw;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import java.net.Socket;
 import java.util.logging.Logger;
 
 public class Main {
@@ -14,10 +15,19 @@ public class Main {
 	public static void main(String[] args) {
 		// Create a Server Socket on port 8080
 		try (ServerSocket server = new ServerSocket(PORT)) {
+			logger.info("Server started on port " + PORT);
+			logger.info("Waiting for a client to connect...");
+
 			// Accept a connection from the server
+			try (Socket client = server.accept()) {
+
+			} catch (IOException ioe) {
+				// Log a warning since the client connection isn't as important
+				logger.warning("[CLIENT]: " + ioe.getMessage());
+			}
 		} catch (IOException ioe) {
 			// Log a severe error since the server cannot be created
-			logger.severe(ioe.getMessage());
+			logger.severe("[SERVER]: " + ioe.getMessage());
 		}
 	}
 }
