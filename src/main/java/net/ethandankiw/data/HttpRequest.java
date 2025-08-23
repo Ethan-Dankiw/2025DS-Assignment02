f0784f95814cf03ec7e978e76f8686166a7349f0
Impolite Goofball <github.dismiss894@passmail.net>
2025-08-23 13:31:03 +0930

    auto-generate getters and setters for the http request object

--- src/main/java/net/ethandankiw/data/HttpRequest.java
+++ src/main/java/net/ethandankiw/data/HttpRequest.java
@@ -3,16 +3,58 @@ package net.ethandankiw.data;
 import java.util.Map;
 
 public class HttpRequest {
+
 	private RequestMethod method;
 	private String path;
 
 	private Map<String, String> headers;
 	private String body;
 
+
 	public HttpRequest() {
 		method = RequestMethod.NONE;
 		path = null;
 		headers = null;
 		body = null;
+	}
+
+
+	public RequestMethod getMethod() {
+		return method;
+	}
+
+
+	public void setMethod(RequestMethod method) {
+		this.method = method;
+	}
+
+
+	public String getPath() {
+		return path;
+	}
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+
+	public Map<String, String> getHeaders() {
+		return headers;
+	}
+
+
+	public void setHeaders(Map<String, String> headers) {
+		this.headers = headers;
+	}
+
+
+	public String getBody() {
+		return body;
+	}
+
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 }
