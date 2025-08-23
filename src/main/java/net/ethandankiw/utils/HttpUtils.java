d77a68884df5128677f28de890aabb0c5e46fb07
Impolite Goofball <github.dismiss894@passmail.net>
2025-08-23 12:05:49 +0930

    parse the headers from the request into a key value map

--- src/main/java/net/ethandankiw/utils/HttpUtils.java
+++ src/main/java/net/ethandankiw/utils/HttpUtils.java
@@ -2,6 +2,9 @@ package net.ethandankiw.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -29,5 +32,38 @@ public class HttpUtils {
 
 		// If invalid request, return empty string
 		return Optional.empty();
+	}
+
+	public static Map<String, String> parseHeaders(BufferedReader fromClient) {
+		// Define the map of header key values paris
+		Map<String, String> headers = new HashMap<>();
+
+		// Loop over the header fields
+		fromClient.lines().forEach(line -> {
+			// If EOF of CRLF is reached
+			if (line == null || line.isBlank()) {
+				return;
+			}
+
+			// Otherwise parse header fields
+			AbstractMap.SimpleEntry<String, String> header = parseSingleHeader(line);
+			headers.put(header.getKey(), header.getValue());
+		});
+
+		// Return the parsed headers
+		return headers;
+	}
+
+	// https://www.baeldung.com/java-pairs
+	private static AbstractMap.SimpleEntry<String, String> parseSingleHeader(String headerLine) {
+		// Split on first colon
+		String[] headerKeyValue = headerLine.split(":", 2);
+
+		// Remove whitespace for each value
+		String key = headerKeyValue[0].trim();
+		String value = headerKeyValue[1].trim();
+
+		// Return a key value pair for the header
+		return new AbstractMap.SimpleEntry<>(key, value);
 	}
 }
